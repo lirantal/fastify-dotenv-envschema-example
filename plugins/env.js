0eec33d1d4226cb25f24de24befd7bea72e73d77
Liran Tal <liran.tal@gmail.com>
2022-12-23 20:58:39 +0200

    feat: add fastify/env which uses dotenv as a configuration provider and adds a schema

--- plugins/env.js
+++ plugins/env.js
@@ -1,11 +1,32 @@
-import * as dotenv from "dotenv";
+import fastifyEnv from "@fastify/env";
 
 export default async function configPlugin(server, options, done) {
-  try {
-    const envConfig = dotenv.config();
-    server.decorate("config", envConfig.parsed);
-    done();
-  } catch (err) {
-    done(err);
-  }
+  const schema = {
+    type: "object",
+    required: ["HTTP_PORT"],
+    properties: {
+      PORT: {
+        type: "number",
+        default: 3001,
+      },
+      DEBUG_LEVEL: {
+        type: "number",
+        default: 1000,
+      },
+    },
+  };
+
+  const configOptions = {
+    // decorate the Fastify server instance with `config` key
+    // such as `fastify.config('PORT')
+    confKey: "config",
+    // schema to validate
+    schema: schema,
+    // source for the configuration data
+    data: process.env,
+    // will read .env in root folder
+    dotenv: true,
+  };
+
+  return fastifyEnv(server, configOptions, done);
 }
